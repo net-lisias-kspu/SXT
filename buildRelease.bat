d501dc3802c3bca7a65be99c57c3c0a0786501a6
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-02-05 06:58:41 -0500

    Deleted unnecessary directory Updated buildRelease.bat

--- buildRelease.bat
+++ buildRelease.bat
@@ -1,4 +1,4 @@
-
+@echo off
 copy SXTContinued.version GameData\SXT
 
 set DEFHOMEDRIVE=d:
@@ -22,8 +22,27 @@ if "%_test%" == ":" (
 set HOMEDRIVE=%HOMEDIR:~0,2%
 )
 
-type SXTContinued.version
-set /p VERSION= "Enter version: "
+
+set VERSIONFILE=SXTContinued.version
+rem The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
+c:\local\jq-win64  ".VERSION.MAJOR" %VERSIONFILE% >tmpfile
+set /P major=<tmpfile
+
+c:\local\jq-win64  ".VERSION.MINOR"  %VERSIONFILE% >tmpfile
+set /P minor=<tmpfile
+
+c:\local\jq-win64  ".VERSION.PATCH"  %VERSIONFILE% >tmpfile
+set /P patch=<tmpfile
+
+c:\local\jq-win64  ".VERSION.BUILD"  %VERSIONFILE% >tmpfile
+set /P build=<tmpfile
+del tmpfile
+set VERSION=%major%.%minor%.%patch%
+if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
+
+
+echo Version:  %VERSION%
+
 
 rmdir /s /q %HOMEDIR%\install\Gamedata\SXTContinued
 
