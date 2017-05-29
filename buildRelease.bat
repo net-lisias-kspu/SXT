54aaf58ed83bd9bd896f0a2787c854a2fb7982f7
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-05-29 10:30:24 -0400

    Updted for 1.3

--- buildRelease.bat
+++ buildRelease.bat
@@ -1,27 +1,9 @@
 @echo off
 copy SXTContinued.version GameData\SXT
 
-set DEFHOMEDRIVE=d:
-set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
-set HOMEDIR=
-set HOMEDRIVE=%CD:~0,2%
 
 set RELEASEDIR=d:\Users\jbb\release
 set ZIP="c:\Program Files\7-zip\7z.exe"
-echo Default homedir: %DEFHOMEDIR%
-
-rem set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
-
-if "%HOMEDIR%" == "" (
-set HOMEDIR=%DEFHOMEDIR%
-)
-echo %HOMEDIR%
-
-SET _test=%HOMEDIR:~1,1%
-if "%_test%" == ":" (
-set HOMEDRIVE=%HOMEDIR:~0,2%
-)
-
 
 set VERSIONFILE=SXTContinued.version
 rem The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
@@ -43,10 +25,6 @@ if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
 
 echo Version:  %VERSION%
 
-
-rmdir /s /q %HOMEDIR%\install\Gamedata\SXTContinued
-
-mkdir %HOMEDIR%\install\Gamedata\SXTContinued
 
 copy ..\MiniAVC.dll Gamedata\SXT
 copy README.md GameData\SXT
