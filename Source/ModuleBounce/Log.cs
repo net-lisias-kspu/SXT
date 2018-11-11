fb71fe127d3770d4739027171e56b61e62db11f5
Lisias T <me@lisias.net>
2018-11-11 02:19:07 -0200

    Adding KSPe Logging facilities

---
+++ Source/ModuleBounce/Log.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+using KSPe.Util.Log;
+
+namespace SXT
+{
+	internal static class Log
+	{
+		private static readonly Logger LOG = Logger.CreateForType<ModuleBounce>();
+
+		internal static void error(string msg, params object[] @params)
+		{
+			LOG.error(msg, @params);
+		}
+
+		internal static void warn(string msg, params object[] @params)
+		{
+			LOG.warn(msg, @params);
+		}
+
+		[Conditional("DEBUG")]	
+		internal static void dbg(string msg, params object[] @params)
+		{
+			LOG.dbg(msg, @params);
+		}
+	}
+}
